bd86c25a20ef6d14caf784d121a447eb8cd13583
Anuraaj <67829724+Anuraaj@users.noreply.github.com>
2020-07-07 10:05:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,4 @@
-/*var wall, car;
 
-var speed, weight;
-
-function setup() 
-{
-  createCanvas(1600, 400);
-  car = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500, 200, 50, 50);
-
-  speed = random(55, 90);
-  weight = random(400, 1500);
-}
-
-function draw()
- {
-  background(255, 255, 255);  
-  drawSprites();
-}*/
 var wall, car;
 
 var speed, weight;
